096434369b7952a1217ef65b1dc922d0db19cb98
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 20:51:15 -0300

    feat: add DeleteTaskController for task deletion

---
+++ src/controllers/delete-task.js
@@ -0,0 +1,25 @@
+import { TaskNotFoundError } from '../errors/index.js';
+import { created, notFound, serverError } from './helpers/http.js';
+
+export class DeleteTaskController {
+  constructor(getTaskByIdUseCase, deleteTaskUseCase) {
+    this.getTaskByIdUseCase = getTaskByIdUseCase;
+    this.deleteTaskUseCase = deleteTaskUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const taskId = httpRequest.params.id;
+
+      const deleteTask = await this.deleteTaskUseCase.execute(taskId);
+
+      return created(deleteTask);
+    } catch (error) {
+      if (error instanceof TaskNotFoundError) {
+        return notFound({ message: error.message });
+      }
+
+      return serverError();
+    }
+  }
+}
