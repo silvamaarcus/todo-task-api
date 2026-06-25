421ca8a947cdc71aa9dd1684b89076c92b48631a
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 11:59:58 -0300

    chore: clean code setup

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+];
