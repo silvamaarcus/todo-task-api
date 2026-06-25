73625a4b762ead187cc6a33db4e3932b57a14c46
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 19:01:34 -0300

    feat: add GetTaskByIdUseCase for retrieving a task by its ID

---
+++ src/use-cases/get-task-by-id.js
@@ -0,0 +1,17 @@
+import { TaskNotFoundError } from '../errors/index.js';
+
+export class GetTaskByIdUseCase {
+  constructor(getTaskByIdRepository) {
+    this.getTaskByIdRepository = getTaskByIdRepository;
+  }
+
+  async execute(taskId) {
+    const taskAlreadyExists = await this.getTaskByIdRepository.execute(taskId);
+
+    if (!taskAlreadyExists) {
+      throw new TaskNotFoundError(taskId);
+    }
+
+    return taskAlreadyExists;
+  }
+}
