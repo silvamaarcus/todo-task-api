2df8d3516add368fab9e8913798bb0f6b63dd9f8
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 20:51:07 -0300

    feat: add UpdateTaskController for updating a task

---
+++ src/controllers/update-task.js
@@ -0,0 +1,37 @@
+import { TaskNotFoundError } from '../errors/index.js';
+import { updateTaskSchema } from '../schemas/task.js';
+import { badRequest, created, notFound, serverError } from './helpers/http.js';
+
+export class UpdateTaskController {
+  constructor(getTaskByIdUseCase, updateTaskUseCase) {
+    this.getTaskByIdUseCase = getTaskByIdUseCase;
+    this.updateTaskUseCase = updateTaskUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      const parsedParams = updateTaskSchema.safeParse(params);
+
+      if (!parsedParams.success) {
+        return badRequest({ message: parsedParams.error.errors[0].message });
+      }
+
+      const taskId = httpRequest.params.id;
+
+      const updateTask = await this.updateTaskUseCase.execute(
+        taskId,
+        parsedParams.data,
+      );
+
+      return created(updateTask);
+    } catch (error) {
+      if (error instanceof TaskNotFoundError) {
+        return notFound({ message: error.message });
+      }
+
+      return serverError();
+    }
+  }
+}
