cb83b94127ef3bce631813357f548ba9a5f032a3
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 20:49:02 -0300

    feat: add notFound, ok functions and apply js docs

--- src/controllers/helpers/http.js
+++ src/controllers/helpers/http.js
@@ -1,3 +1,22 @@
+/**
+ *  Função para retornar uma resposta HTTP com status 200 (OK).
+ *  Indica que a requisição foi bem-sucedida e o recurso solicitado foi encontrado.
+ *  @param {any} body - O corpo da resposta.
+ *  @returns {object} - Objeto contendo o statusCode e o corpo da resposta.
+ */
+export const ok = (body) => {
+  return {
+    statusCode: 200,
+    body,
+  };
+};
+
+/**
+ *  Função para retornar uma resposta HTTP com status 201 (Created).
+ *  Indica que a requisição foi bem-sucedida e um novo recurso foi criado.
+ *  @param {any} body - O corpo da resposta.
+ *  @returns {object} - Objeto contendo o statusCode e o corpo da resposta.
+ */
 export const created = (body) => {
   return {
     statusCode: 201,
@@ -5,6 +24,12 @@ export const created = (body) => {
   };
 };
 
+/**
+ * Função para retornar uma resposta HTTP com status 400 (Bad Request).
+ * Indica que a requisição foi mal formulada ou contém erros nos parâmetros.
+ * @param {any} body - O corpo da resposta.
+ * @returns {object} - Objeto contendo o statusCode e o corpo da resposta.
+ */
 export const badRequest = (body) => {
   return {
     statusCode: 400,
@@ -12,6 +37,25 @@ export const badRequest = (body) => {
   };
 };
 
+/**
+ * Função para retornar uma resposta HTTP com status 404 (Not Found).
+ * Indica que o recurso solicitado não foi encontrado no servidor.
+ * @param {any} body - O corpo da resposta.
+ * @returns {object} - Objeto contendo o statusCode e o corpo da resposta.
+ */
+export const notFound = (body) => {
+  return {
+    statusCode: 404,
+    body,
+  };
+};
+
+/**
+ * Função para retornar uma resposta HTTP com status 500 (Internal Server Error).
+ * Indica que ocorreu um erro interno no servidor ao processar a requisição.
+ * @param {any} body - O corpo da resposta.
+ * @returns {object} - Objeto contendo o statusCode e o corpo da resposta.
+ */
 export const serverError = () => {
   return {
     statusCode: 500,
