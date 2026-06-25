956ec5928cd064cd34282c54ac24214b7a39e229
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 17:14:19 -0300

    feat: add GetTaskByIdRepository for retrieving a task by its ID

---
+++ src/repositories/postgres/get-task-by-id.js
@@ -0,0 +1,13 @@
+import { prisma } from '../../../prisma/prisma.js';
+
+export class GetTaskByIdRepository {
+  async execute(taskId) {
+    const task = await prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    return task;
+  }
+}
