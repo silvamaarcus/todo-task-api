3e053cf72f1bfbe7a9da355cece33298e2a4bde8
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-25 17:14:32 -0300

    feat: add UpdateTaskRepository for updating a task by its ID

---
+++ src/repositories/postgres/update-task.js
@@ -0,0 +1,29 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+import { prisma } from '../../../prisma/prisma';
+import { TaskNotFoundError } from '../../errors/index.js';
+
+export class UpdateTaskRepository {
+  async execute(taskId, updateTaskParams) {
+    try {
+      const task = await prisma.task.update({
+        where: {
+          id: taskId,
+        },
+        data: updateTaskParams,
+      });
+
+      return task;
+    } catch (error) {
+      // Erro específico do Prisma
+      if (error instanceof PrismaClientKnownRequestError) {
+        // P2025 -> é o código de erro do Prisma que indica que o registro (task) não foi encontrado.
+        if (error.code === 'P2025') {
+          throw new TaskNotFoundError(taskId);
+        }
+      }
+
+      throw error; // Para outros erros, relança o erro.
+    }
+  }
+}
