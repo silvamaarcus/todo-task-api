5c1171a4349f2d99a11682c5d3f8fcc6b5147358
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-27 00:01:05 -0300

    test: add CreateTaskRepository test for task creation

--- src/repositories/postgres/create-task.test.js
+++ src/repositories/postgres/create-task.test.js
@@ -1,3 +1,15 @@
+import { task } from '../../tests/fixtures/tasks';
+import { CreateTaskRepository } from './create-task';
+
 describe('CreateTaskRepository', () => {
-  test('Deve salvar a Task no banco de dados', () => {});
+  test('Deve salvar a Task no banco de dados', async () => {
+    const sut = new CreateTaskRepository();
+
+    const createTask = await sut.execute(task);
+
+    expect(createTask.id).toBeDefined();
+    expect(createTask.title).toBe('Teste');
+    expect(createTask.description).toBe('Comentário...');
+    expect(createTask.status).toBe('TODO');
+  });
 });
