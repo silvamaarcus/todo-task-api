f6bcedc8d0a95fca1d882bb6ac9d7b06dd614db3
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-27 16:21:45 -0300

    test: ensure GetAllTasksController return 200 when get all tasks

---
+++ src/controllers/get-all-tasks.test.js
@@ -0,0 +1,32 @@
+import { task } from '../tests/fixtures/tasks';
+import { GetAllTasksController } from './get-all-tasks';
+
+describe('GetAllTasksController', () => {
+  class GetAllTasksTaskUseCaseStub {
+    async execute() {
+      return task;
+    }
+  }
+
+  const makeSut = () => {
+    const getAllTasksTaskUseCaseStub = new GetAllTasksTaskUseCaseStub();
+    const sut = new GetAllTasksController(getAllTasksTaskUseCaseStub);
+
+    return {
+      getAllTasksTaskUseCaseStub,
+      sut,
+    };
+  };
+
+  const httpRequest = {
+    body: task,
+  };
+
+  test('Deve retornar 200 quando as Taks forem encontradas', async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(200);
+  });
+});
