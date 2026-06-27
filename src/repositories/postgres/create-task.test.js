62c13cacce3a96950972a46c0feeccf50cb8be0f
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-27 00:47:14 -0300

    test: ensure CreateTaskRepository throws error when just try save task

--- src/repositories/postgres/create-task.test.js
+++ src/repositories/postgres/create-task.test.js
@@ -1,5 +1,6 @@
-import { task } from '../../tests/fixtures/tasks';
-import { CreateTaskRepository } from './create-task';
+import { prisma } from '../../../prisma/prisma.js';
+import { task } from '../../tests/fixtures/tasks.js';
+import { CreateTaskRepository } from './create-task.js';
 
 describe('CreateTaskRepository', () => {
   test('Deve salvar a Task no banco de dados', async () => {
@@ -11,5 +12,16 @@ describe('CreateTaskRepository', () => {
     expect(createTask.title).toBe('Teste');
     expect(createTask.description).toBe('Comentário...');
     expect(createTask.status).toBe('TODO');
+  });
+
+  test('Deve garantir que o Prisma lançe uma exceção caso não consiga salvar', async () => {
+    const sut = new CreateTaskRepository();
+    import.meta.jest
+      .spyOn(prisma.task, 'create')
+      .mockImplementationOnce(new Error()); // Simula um erro no método create
+
+    const promise = sut.execute(task);
+
+    await expect(promise).rejects.toThrow(); // Lança uma exceção
   });
 });
