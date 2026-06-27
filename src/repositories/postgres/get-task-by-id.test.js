55d9dff9406d9618969cd5efc35a119647adf94d
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-27 12:25:16 -0300

    feat: ensure GetTaskByIdRepository to handle exceptions

--- src/repositories/postgres/get-task-by-id.test.js
+++ src/repositories/postgres/get-task-by-id.test.js
@@ -1,3 +1,6 @@
+import { faker } from '@faker-js/faker';
+
+import { prisma } from '../../../prisma/prisma';
 import { task } from '../../tests/fixtures/tasks';
 import { CreateTaskRepository } from './create-task';
 import { GetTaskByIdRepository } from './get-task-by-id';
@@ -12,5 +15,16 @@ describe('GetTaskByIdRepository', () => {
 
     expect(result.id).toBeDefined();
     expect(result.title).toBe('Teste');
+  });
+
+  test('Deve garantir que o Prisma lançe uma exceção caso não consiga encontrar', async () => {
+    const sut = new GetTaskByIdRepository();
+    import.meta.jest
+      .spyOn(prisma.task, 'findUnique')
+      .mockRejectedValueOnce(new Error('Erro de teste'));
+
+    const promise = sut.execute(faker.string.uuid());
+
+    await expect(promise).rejects.toThrow(); // Lança uma exceção
   });
 });
