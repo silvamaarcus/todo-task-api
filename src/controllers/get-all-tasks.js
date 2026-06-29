6fb922686612e972139126332eae98bbf972dd68
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-29 10:36:40 -0300

    feat: enhance GetAllTasksController to validate status query parameter

--- src/controllers/get-all-tasks.js
+++ src/controllers/get-all-tasks.js
@@ -1,13 +1,27 @@
-import { ok, serverError } from './helpers/http.js';
+import { TASK_TYPE } from '../constants/index.js';
+import { badRequest, ok, serverError } from './helpers/http.js';
 
 export class GetAllTasksController {
   constructor(getAllTasksTaskUseCase) {
     this.getAllTasksTaskUseCase = getAllTasksTaskUseCase;
   }
 
-  async execute() {
+  async execute(httpRequest) {
     try {
-      const tasks = await this.getAllTasksTaskUseCase.execute();
+      const { status } = httpRequest.query;
+
+      if (
+        status &&
+        status !== TASK_TYPE.TODO &&
+        status !== TASK_TYPE.IN_PROGRESS &&
+        status !== TASK_TYPE.DONE
+      ) {
+        return badRequest({
+          message: `Status inválido! Escolha: ${TASK_TYPE.TODO}, ${TASK_TYPE.IN_PROGRESS} ou ${TASK_TYPE.DONE}`,
+        });
+      }
+
+      const tasks = await this.getAllTasksTaskUseCase.execute({ status });
 
       return ok(tasks);
     } catch (error) {
