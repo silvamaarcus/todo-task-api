4e69eda4aeee695cae9689206c88df707a6bae2c
Marcus Silva <devsilvamarcus@gmail.com>
2026-06-29 10:49:40 -0300

    test: update GetAllTasksRepository test to use fixture tasks and validate task count

--- src/repositories/postgres/get-all-tasks.test.js
+++ src/repositories/postgres/get-all-tasks.test.js
@@ -1,29 +1,24 @@
-import { faker } from '@faker-js/faker';
-
-import { task } from '../../tests/fixtures/tasks.js';
+import { tasks } from '../../tests/fixtures/tasks.js';
 import { CreateTaskRepository } from './create-task';
 import { GetAllTasksRepository } from './get-all-tasks.js';
 
 describe('GetAllTasksRepository', () => {
   test('Deve buscar todas as Tasks no banco', async () => {
-    const task2 = {
-      id: faker.string.uuid(),
-      title: 'Teste 2',
-      description: 'Comentário 2...',
-      status: 'TODO',
-    };
-
     // Cria duas Tasks no banco de dados
     const createRepository = new CreateTaskRepository();
-    await createRepository.execute(task);
-    await createRepository.execute(task2);
+    await createRepository.execute(tasks[0]);
+    await createRepository.execute(tasks[1]);
+    await createRepository.execute(tasks[2]);
 
     // Buscar todas as Tasks
     const sut = new GetAllTasksRepository();
     const getAllTasks = await sut.execute();
 
-    expect(getAllTasks).toHaveLength(2); // Verifica se o número de Tasks retornadas é 2
-    expect(getAllTasks[0].title).toBe('Teste'); // Verifica se o título da primeira Task é "Teste"
-    expect(getAllTasks[1].title).toBe('Teste 2'); // Verifica se o título da segunda Task é "Teste 2"
+    console.log(getAllTasks);
+
+    expect(getAllTasks).toHaveLength(3); // Verifica se o número de Tasks retornadas é 2
+    expect(getAllTasks[0].title).toBe('Tarefa teste 1');
+    expect(getAllTasks[1].title).toBe('Tarefa teste 2');
+    expect(getAllTasks[2].title).toBe('Tarefa teste 3');
   });
 });
