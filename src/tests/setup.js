52c00560cdd9661ee56ba16e4a90976a4a95b214
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 01:36:26 -0300

    fix: ensure user deletion is performed in beforeEach and afterEach hooks

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,9 +1,11 @@
 import { prisma } from '../../prisma/prisma.js';
 
 beforeEach(async () => {
+  await prisma.user.deleteMany();
   await prisma.task.deleteMany();
 });
 
 afterEach(async () => {
+  await prisma.user.deleteMany();
   await prisma.task.deleteMany();
 });
