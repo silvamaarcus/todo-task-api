72d2ea7de894d89a8f2027b9d8cdc2cfb11f5c89
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 18:34:30 -0300

    feat: add createUserSchema for user validation

---
+++ src/schemas/user.js
@@ -0,0 +1,21 @@
+import z from 'zod';
+
+export const createUserSchema = z.object({
+  name: z
+    .string('O nome é obrigatório.')
+    .trim()
+    .min(1, { message: 'Nome deve conter no mínimo 1 caractere.' }),
+  last_name: z
+    .string('O sobrenome é obrigatório.')
+    .trim()
+    .min(1, { message: 'Sobrenome deve conter no mínimo 1 caractere.' }),
+  email: z
+    .string('O email é obrigatório.')
+    .trim()
+    .email('Formato de e-mail inválido, tente novamente.')
+    .min(1, { message: 'Email deve conter no mínimo 1 caractere.' }),
+  password: z
+    .string('A senha é obrigatória.')
+    .trim()
+    .min(6, { message: 'Senha deve conter no mínimo 6 caracteres.' }),
+});
