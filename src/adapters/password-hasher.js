25be01388fca1090ecc56adc04a02ccafe7ddb64
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 00:11:29 -0300

    feat: add PasswordHasherAdapter for password hashing functionality

---
+++ src/adapters/password-hasher.js
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt';
+
+export class PasswordHasherAdapter {
+  execute(password) {
+    return bcrypt.hash(password, 10); // O 10 é o número de rounds de hashing: Exem
+  }
+}
