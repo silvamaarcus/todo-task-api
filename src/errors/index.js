5fc95803fc657786fd244d9fae1628c4a77d4d61
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 02:02:48 -0300

    feat: add UserNotFoundError class for user-related error handling

--- src/errors/index.js
+++ src/errors/index.js
@@ -4,3 +4,10 @@ export class TaskNotFoundError extends Error {
     this.name = 'TaskNotFoundError';
   }
 }
+
+export class UserNotFoundError extends Error {
+  constructor(userId) {
+    super(`Usuário com ID "${userId}" não encontrada.`); // Ao usar uma classe extendida, é obrigatório usar o 'super'
+    this.name = 'UserNotFoundError';
+  }
+}
