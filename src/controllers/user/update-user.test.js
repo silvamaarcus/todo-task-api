2c70dbe5d34fe82be32e8b9bd1cb2c904158608c
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 22:01:42 -0300

    test: add unit tests for UpdateUserController to validate user update scenarios

---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,79 @@
+import {
+  EmailAlreadyInUseError,
+  UserNotFoundError,
+} from '../../errors/index.js';
+import { user } from '../../tests/fixtures/users.js';
+import { UpdateUserController } from './update-user.js';
+
+describe('UpdateUserController', () => {
+  class UpdateUserUseCaseStub {
+    async execute() {
+      return user;
+    }
+  }
+
+  const makeSut = () => {
+    const updateUserUseCaseStub = new UpdateUserUseCaseStub();
+    const sut = new UpdateUserController(updateUserUseCaseStub);
+
+    return {
+      sut,
+      updateUserUseCaseStub,
+    };
+  };
+
+  const httpRequest = {
+    params: {
+      id: user.id,
+    },
+    body: user,
+  };
+
+  test('Deve retornar 200 quando um usuário for atualizado com sucesso', async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toEqual(httpRequest.body);
+  });
+
+  test('Deve retornar 400 quando ocorrer erro EmailAlreadyInUseError', async () => {
+    const { sut, updateUserUseCaseStub } = makeSut();
+    import.meta.jest
+      .spyOn(updateUserUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new EmailAlreadyInUseError('email@aws.com'));
+    const httpRequestAlt = {
+      params: {
+        id: user.id,
+      },
+      body: { ...user, email: 'email@aws.com' },
+    };
+
+    const result = await sut.execute(httpRequestAlt);
+
+    expect(result.statusCode).toBe(400);
+  });
+
+  test('Deve retornar 404 quando ocorrer erro UserNotFoundError', async () => {
+    const { sut, updateUserUseCaseStub } = makeSut();
+    import.meta.jest
+      .spyOn(updateUserUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new UserNotFoundError('email@aws.com'));
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(404);
+  });
+
+  test('Deve retornar 500 quando ocorrer erro interno no servidor', async () => {
+    const { sut, updateUserUseCaseStub } = makeSut();
+    import.meta.jest
+      .spyOn(updateUserUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(500);
+  });
+});
