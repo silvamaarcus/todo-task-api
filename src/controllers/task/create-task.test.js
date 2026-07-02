cb6573d2e813a23df1beb4351ac9d9861abe9d0c
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-02 20:24:51 -0300

    test: add user_id in CreateTaskController for validation on create task

--- src/controllers/task/create-task.test.js
+++ src/controllers/task/create-task.test.js
@@ -1,5 +1,5 @@
-import { task } from '../../tests/fixtures/tasks';
-import { CreateTaskController } from './create-task';
+import { task } from '../../tests/fixtures/tasks.js';
+import { CreateTaskController } from './create-task.js';
 
 describe('CreateTaskController', () => {
   class CreateTaskUseCaseStub {
@@ -65,10 +65,11 @@ describe('CreateTaskController', () => {
 
     expect(executeSpy).toHaveBeenCalledTimes(1); // Verifica se o método foi chamado apenas uma vez
     expect(executeSpy).toHaveBeenCalledWith({
+      user_id: httpRequest.body.user_id,
       title: httpRequest.body.title,
       description: httpRequest.body.description,
       status: httpRequest.body.status,
-    }); // Zod valida title, description e status
+    }); // Zod valida user_id, title, description e status
   });
 
   test('Deve retornar 500 quando ocorrer erro interno no servidor', async () => {
