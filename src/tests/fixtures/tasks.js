e83a0eb8e4bbe3d96ed4da662e34a0d2dcea617c
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-02 20:23:12 -0300

    feat: add user_id to task fixtures for consistency

--- src/tests/fixtures/tasks.js
+++ src/tests/fixtures/tasks.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 export const task = {
+  user_id: '59763d79-fee1-4bdb-ab27-ff8a690744fa',
   id: faker.string.uuid(),
   title: 'Teste',
   description: 'Comentário...',
@@ -8,6 +9,7 @@ export const task = {
 };
 
 export const newTask = {
+  user_id: '59763d79-fee1-4bdb-ab27-ff8a690744fa',
   id: faker.string.uuid(),
   title: 'Novo Teste',
   description: 'Comentário editato!',
@@ -16,18 +18,21 @@ export const newTask = {
 
 export const tasks = [
   {
+    user_id: '59763d79-fee1-4bdb-ab27-ff8a690744fa',
     id: faker.string.uuid(),
     title: 'Tarefa teste 1',
     description: 'Descrição da tarefa teste 1',
     status: 'TODO',
   },
   {
+    user_id: '59763d79-fee1-4bdb-ab27-ff8a690744fa',
     id: faker.string.uuid(),
     title: 'Tarefa teste 2',
     description: 'Descrição da tarefa teste 2',
     status: 'IN_PROGRESS',
   },
   {
+    user_id: '59763d79-fee1-4bdb-ab27-ff8a690744fa',
     id: faker.string.uuid(),
     title: 'Tarefa teste 3',
     description: 'Descrição da tarefa teste 3',
