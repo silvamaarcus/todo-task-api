45d5c941e256c802ddb1e08ebb009cb2be3e7775
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-01 21:22:08 -0300

    feat: update user exports to include get-user-by-id instead of get-user-by-email

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -5,5 +5,5 @@ export * from './task/get-task-by-id.js';
 export * from './task/update-task.js';
 export * from './user/create-user.js';
 export * from './user/delete-user.js';
-export * from './user/get-user-by-email.js';
+export * from './user/get-user-by-id.js';
 export * from './user/update-user.js';
