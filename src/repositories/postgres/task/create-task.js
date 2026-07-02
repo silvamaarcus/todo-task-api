6db695f564494cd66a6a35efcb40f30a5dab07da
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-02 20:18:42 -0300

    feat: add user_id to task creation data

--- src/repositories/postgres/task/create-task.js
+++ src/repositories/postgres/task/create-task.js
@@ -4,6 +4,7 @@ export class CreateTaskRepository {
   async execute(createTaskParams) {
     const task = await prisma.task.create({
       data: {
+        user_id: createTaskParams.user_id,
         id: createTaskParams.id,
         title: createTaskParams.title,
         description: createTaskParams.description,
