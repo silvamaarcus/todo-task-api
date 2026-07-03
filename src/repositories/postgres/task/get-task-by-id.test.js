a07f4faf2f1d3f10f12756b8ef26c562c645ee36
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-02 23:36:41 -0300

    test: add user_id parameter to GetTaskByIdRepository for user-specific task retrieval

--- src/repositories/postgres/task/get-task-by-id.test.js
+++ src/repositories/postgres/task/get-task-by-id.test.js
@@ -1,18 +1,30 @@
 import { faker } from '@faker-js/faker';
 
 import { prisma } from '../../../../prisma/prisma.js';
-import { task } from '../../../tests/fixtures/tasks';
-import { CreateTaskRepository } from './create-task';
-import { GetTaskByIdRepository } from './get-task-by-id';
+import { task } from '../../../tests/fixtures/tasks.js';
+import { user } from '../../../tests/fixtures/users.js';
+import { CreateUserRepository } from '../user/create-user.js';
+import { CreateTaskRepository } from './create-task.js';
+import { GetTaskByIdRepository } from './get-task-by-id.js';
 
 describe('GetTaskByIdRepository', () => {
-  test('Deve buscar uma Task pelo ID', async () => {
+  test('Deve buscar uma Task pelo ID na db', async () => {
+    // Cria um usuário com o mesmo ID que a task espera
+    const createUserRepository = new CreateUserRepository();
+    const userId = faker.string.uuid();
+    await createUserRepository.execute({
+      ...user,
+      id: userId, // Garante que o user_id seja o mesmo da task
+    });
+
+    // Cria uma Task no banco de dados
     const createTask = new CreateTaskRepository();
-    await createTask.execute(task);
+    await createTask.execute({ ...task, user_id: userId });
 
     const sut = new GetTaskByIdRepository();
     const result = await sut.execute(task.id);
 
+    expect(result.user_id).toBeDefined();
     expect(result.id).toBeDefined();
     expect(result.title).toBe('Teste');
   });
@@ -23,7 +35,7 @@ describe('GetTaskByIdRepository', () => {
       .spyOn(prisma.task, 'findUnique')
       .mockRejectedValueOnce(new Error('Erro de teste'));
 
-    const promise = sut.execute(faker.string.uuid());
+    const promise = sut.execute('');
 
     await expect(promise).rejects.toThrow(); // Lança uma exceção
   });
