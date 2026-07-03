8f45281a931536ae78abb2821670fe6a31819aa6
Marcus Silva <devsilvamarcus@gmail.com>
2026-07-02 21:38:28 -0300

    test: refactor GetAllTasksRepository test to use user_id for task creation

--- src/repositories/postgres/task/get-all-tasks.test.js
+++ src/repositories/postgres/task/get-all-tasks.test.js
@@ -1,22 +1,37 @@
-import { tasks } from '../../../tests/fixtures/tasks.js';
-import { CreateTaskRepository } from './create-task';
+import { faker } from '@faker-js/faker';
+
+import {
+  task_done,
+  task_in_progress,
+  task_todo,
+} from '../../../tests/fixtures/tasks.js';
+import { user } from '../../../tests/fixtures/users.js';
+import { CreateUserRepository } from '../user/create-user.js';
+import { CreateTaskRepository } from './create-task.js';
 import { GetAllTasksRepository } from './get-all-tasks.js';
 
 describe('GetAllTasksRepository', () => {
   test('Deve buscar todas as Tasks no banco', async () => {
-    // Cria duas Tasks no banco de dados
+    // Cria um usuário com o mesmo ID que a task espera
+    const createUserRepository = new CreateUserRepository();
+    const userId = faker.string.uuid();
+    await createUserRepository.execute({
+      ...user,
+      id: userId, // Garante que o user_id seja o mesmo da task
+    });
+
+    // Cria Tasks no banco de dados
     const createRepository = new CreateTaskRepository();
-    await createRepository.execute(tasks[0]);
-    await createRepository.execute(tasks[1]);
-    await createRepository.execute(tasks[2]);
+    await createRepository.execute({ ...task_todo, user_id: userId });
+    await createRepository.execute({ ...task_in_progress, user_id: userId });
+    await createRepository.execute({ ...task_done, user_id: userId });
+    const filters = { user_id: userId }; // Filtro para buscar todas as Tasks do usuário
 
     // Buscar todas as Tasks
     const sut = new GetAllTasksRepository();
-    const getAllTasks = await sut.execute();
+    const getAllTasks = await sut.execute(filters);
 
-    console.log(getAllTasks);
-
-    expect(getAllTasks).toHaveLength(3); // Verifica se o número de Tasks retornadas é 2
+    expect(getAllTasks).toHaveLength(3); // Verifica se o número de Tasks retornadas é 3
     expect(getAllTasks[0].title).toBe('Tarefa teste 1');
     expect(getAllTasks[1].title).toBe('Tarefa teste 2');
     expect(getAllTasks[2].title).toBe('Tarefa teste 3');
